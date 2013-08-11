4176c27e38efea3fde939d7c49c37cebddbdd526
Kevin Noppers <Kevin@iMac-van-Kevin.local>
2013-08-11 19:05:34 +0200

    Stylesheet gekoppeld, indeling in code duidelijker neergezet

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <!doctype html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Naamloos document</title>
-</head>
-
-<body>
-<div id="container">
-	<div id="header">
-		<p>To Do List</p>
-	</div>
-	<div id="content">
-		<div id="item">
-			<div id="vink"></div>
-			<div id="opdracht">Website maken</div>
-			<div id="verwijder"></div>
+	<head>
+		<meta charset="UTF-8">
+		<title>Naamloos document</title>
+		<link href="style.css" rel="stylesheet">
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<p>To Do List</p>
+			</div>
+			<div id="content">
+				<div id="item">
+					<div id="vink"></div>
+					<div id="opdracht">Website maken</div>
+					<div id="verwijder"></div>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
-</body>
+	</body>
 </html>
