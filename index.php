a18ccc196cd8b456706b34ee8f39a929274e7a27
kn379 <kevinkevinjr@hotmail.com>
2013-08-24 10:41:15 +0200

    Volledige opmaak aangepast

--- index.php
+++ index.php
@@ -1,19 +1,43 @@
+<?php session_start();
+
+$_SESSION['var'] = $variabele;
+
+ ?>
 <!doctype html>
 <html>
 	<head>
 		<meta charset="UTF-8">
 		<title>Naamloos document</title>
 		<link href="style.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Bad+Script' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 		<div id="container">
 			<div id="header">
-				<p>To Do List</p>
 			</div>
 			<div id="content">
+            				<p>To Do List</p>
+
 				<div id="item">
-					<div id="vink"></div>
-					<div id="opdracht">Website maken</div>
+					<div id="vink"></div><div id="opdracht">Website maken</div>
+					<div id="verwijder"></div>
+					<div id="lijn"></div>
+				</div>
+
+				<div id="item">
+					<div id="vink"></div><div id="opdracht">Boodschappen doen</div>
+					<div id="verwijder"></div>
+					<div id="lijn"></div>
+				</div>
+
+				<div id="item">
+					<div id="vink"></div><div id="opdracht">Dieren wassen</div>
+					<div id="verwijder"></div>
+					<div id="lijn"></div>
+				</div>
+
+				<div id="item">
+					<div id="vink"></div><div id="opdracht">Paardenstal schoonmaken en hooien</div>
 					<div id="verwijder"></div>
 					<div id="lijn"></div>
 				</div>
