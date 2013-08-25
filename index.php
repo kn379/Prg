41777714cac52155dbc3ce3bb061b2bfe62727ef
kn379 <kevinkevinjr@hotmail.com>
2013-08-25 16:50:12 +0200

    Volledige opmaak toegepast op een item die geupdate wordt vanuit de array.

--- index.php
+++ index.php
@@ -37,38 +37,11 @@ echo "<p>To Do List</p>";
 
 for ($row = 0; $row < 4; $row++)
 {
-	echo "<div id='item'><div id='opdracht'>";
+	echo "<div id='item'><div id='vink'></div><div id='opdracht'>";
     echo $shop[$row]["Beschrijving"]." is klaar? ".$shop[$row]["Done"].", ".$shop[$row]["Number"];
-    echo "</div></div>";
+    echo "</div><div id='verwijder'></div> <div id='lijn'></div></div>";
 }
  ?>
-			
-            				<p>To Do List</p>
-			
-
-				<div id="item">
-					<div id="vink"></div><div id="opdracht">Website maken</div>
-					<div id="verwijder"></div>
-					<div id="lijn"></div>
-				</div>
-
-				<div id="item">
-					<div id="vink"></div><div id="opdracht">Boodschappen doen</div>
-					<div id="verwijder"></div>
-					<div id="lijn"></div>
-				</div>
-
-				<div id="item">
-					<div id="vink"></div><div id="opdracht">Dieren wassen</div>
-					<div id="verwijder"></div>
-					<div id="lijn"></div>
-				</div>
-
-				<div id="item">
-					<div id="vink"></div><div id="opdracht">Paardenstal schoonmaken en hooien</div>
-					<div id="verwijder"></div>
-					<div id="lijn"></div>
-				</div>
 			</div>
 		</div>
 	</body>
