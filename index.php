7995dbcdc37f9603c85d5e953b42682f22a87acd
kn379 <kevinkevinjr@hotmail.com>
2013-08-25 16:22:31 +0200

    Vanuit een array de to do items toevoegen.

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<?php session_start();
 
-$_SESSION['var'] = $variabele;
-
- ?>
 <!doctype html>
 <html>
 	<head>
@@ -16,7 +12,39 @@ $_SESSION['var'] = $variabele;
 			<div id="header">
 			</div>
 			<div id="content">
+			
+			<?php session_start();
+
+
+$shop = array( array( Beschrijving => "Website maken", 
+                      Done => ja,
+                      Number => nee 
+                    ),
+               array( Beschrijving => "Boodschappen doen", 
+                      Done => ja,
+                      Number => nee,
+                    ),
+               array( Beschrijving => "Dieren wassen", 
+                      Done => ja,
+                      Number => nee 
+                    ),
+               array( Beschrijving => "Paardenstal schoonmaken en hooien", 
+                      Done => ja,
+                      Number => nee 
+                    )
+             );             
+echo "<p>To Do List</p>";
+
+for ($row = 0; $row < 4; $row++)
+{
+	echo "<div id='item'><div id='opdracht'>";
+    echo $shop[$row]["Beschrijving"]." is klaar? ".$shop[$row]["Done"].", ".$shop[$row]["Number"];
+    echo "</div></div>";
+}
+ ?>
+			
             				<p>To Do List</p>
+			
 
 				<div id="item">
 					<div id="vink"></div><div id="opdracht">Website maken</div>
