4d38defb0aa6eaa060fb1790b7c2f691113dc615
kn379 <kevinkevinjr@hotmail.com>
2013-09-04 20:17:21 +0200

    Toevoegen, verwijderen, afvinken, aanpassen.

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php session_start();
-
-$_SESSION['var'] = $variabele;
-
- ?>
+	if (!isset($_SESSION['todo']) || !isset($_SESSION['todo_newId'])) {
+    	 $_SESSION['todo'] = array();
+    	 $_SESSION['todo_newId'] = 0;
+    	 }
+    if (isset($_POST['action'])) {
+    	switch ($_POST['action']) {
+          case "new":
+               $newId = (++$_SESSION['todo_newId']);
+               $_SESSION['todo'][$newId] = array("txt"=>$_POST['txt'], "done"=>false); 
+               break;
+          case "Bewerken":
+               $id = $_POST['id'];                               
+               $_SESSION['todo'][$id] = array("txt"=>$_POST['txt'], "done"=>isset($_POST['done']));
+               break;
+          case "Verwijderen":
+               $id = $_POST['id'];                                 
+               unset($_SESSION['todo'][$id]);                     
+               break;
+          default:
+          }
+     } 
+?>
 <!doctype html>
 <html>
 	<head>
@@ -19,16 +37,24 @@ $_SESSION['var'] = $variabele;
             				<p>To Do List</p>
 
 				<div id="item">
-					<div id="vink"></div><div id="opdracht">Website maken</div>
-					<div id="verwijder"></div>
-					<div id="lijn"></div>
+					<form action="index.php" method="post">
+						<input type="hidden" name="action" value="new">
+						Nieuwe taak: <input type="text" name="txt"><input type="submit" value="Toevoegen">
+					</form>
 				</div>
 
-				<div id="item">
-					<div id="vink"></div><div id="opdracht">Boodschappen doen</div>
-					<div id="verwijder"></div>
-					<div id="lijn"></div>
-				</div>
+					<?php foreach ($_SESSION['todo'] as $todoId=>$todo) {?>
+					<div id="item">
+						<form action="index.php" method="post">
+							<input type="hidden" name="id" value="<?php echo $todoId; ?>">
+							<input type="checkbox" name="done" value="1"<?php if ($todo['done']==true) echo " checked"; ?>>
+							<input type="text" name="txt" value="<?php echo htmlspecialchars($todo['txt']); ?>">
+							<input type="submit" name="action" value="Bewerken"><input type="submit" name="action" value="Verwijderen">
+						</form></div>
+					<?php
+						}
+					?>
+				
 
 				<div id="item">
 					<div id="vink"></div><div id="opdracht">Dieren wassen</div>
