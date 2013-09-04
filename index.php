96fe6b2c2e858df19f9edfe1b2950862c818907f
kn379 <kevinkevinjr@hotmail.com>
2013-09-04 20:08:57 +0200

    Revert "Volledige opmaak aangepast"
    This reverts commit 7995dbcdc37f9603c85d5e953b42682f22a87acd.
    
    Conflicts:
    	index.php
    	style.css

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
+<?php session_start();
+
+$_SESSION['var'] = $variabele;
+
+ ?>
+<!doctype html>
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>To Do List</title>
+		<title>Naamloos document</title>
 		<link href="style.css" rel="stylesheet">
         <link href='http://fonts.googleapis.com/css?family=Bad+Script' rel='stylesheet' type='text/css'>
 	</head>
@@ -10,33 +16,31 @@
 			<div id="header">
 			</div>
 			<div id="content">
-			
-			<?php session_start();
+            				<p>To Do List</p>
 
+				<div id="item">
+					<div id="vink"></div><div id="opdracht">Website maken</div>
+					<div id="verwijder"></div>
+					<div id="lijn"></div>
+				</div>
 
-$shop = array( array( Beschrijving => "Website maken", 
-                      Done => 0,
-                    ),
-               array( Beschrijving => "Boodschappen doen", 
-                      Done => 1,
-                    ),
-               array( Beschrijving => "Dieren wassen", 
-                      Done => 1,
-                    ),
-               array( Beschrijving => "Paardenstal schoonmaken en hooien", 
-                      Done => 1,
-                    )
-             );             
-echo "<p>To Do List</p>";
+				<div id="item">
+					<div id="vink"></div><div id="opdracht">Boodschappen doen</div>
+					<div id="verwijder"></div>
+					<div id="lijn"></div>
+				</div>
 
-for ($row = 0; $row < 4; $row++)
-{
-	echo "<div id='item'>";
-    if ($shop[$row]["Done"] < 1) {echo "<div id='vink'></div>";} else { echo "<div id='verwijder'></div>";}
-    echo "<div id='opdracht'>".$shop[$row]["Beschrijving"];
-	echo "</div> <div id='lijn'></div></div>";
-			}
- ?>
+				<div id="item">
+					<div id="vink"></div><div id="opdracht">Dieren wassen</div>
+					<div id="verwijder"></div>
+					<div id="lijn"></div>
+				</div>
+
+				<div id="item">
+					<div id="vink"></div><div id="opdracht">Paardenstal schoonmaken en hooien</div>
+					<div id="verwijder"></div>
+					<div id="lijn"></div>
+				</div>
 			</div>
 		</div>
 	</body>
