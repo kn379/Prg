aeee045effb875faf9a9bdacea3790554e7c5d7f
kn379 <kevinkevinjr@hotmail.com>
2013-09-04 20:43:18 +0200

    Code volledig werkend gemaakt, deel opmaak klaar. Verder de actie voor het opslaan van de aanpassingen aangepast van bewerken naar opslaan

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
                $newId = (++$_SESSION['todo_newId']);
                $_SESSION['todo'][$newId] = array("txt"=>$_POST['txt'], "done"=>false); 
                break;
-          case "Bewerken":
+          case "Opslaan":
                $id = $_POST['id'];                               
                $_SESSION['todo'][$id] = array("txt"=>$_POST['txt'], "done"=>isset($_POST['done']));
                break;
@@ -39,7 +39,8 @@
 				<div id="item">
 					<form action="index.php" method="post">
 						<input type="hidden" name="action" value="new">
-						Nieuwe taak: <input type="text" name="txt"><input type="submit" value="Toevoegen">
+						Nieuwe taak: <input type="text" name="txt">
+						<input type="submit" value="Toevoegen">
 					</form>
 				</div>
 
@@ -49,9 +50,9 @@
 							<input type="hidden" name="id" value="<?php echo $todoId; ?>">
 							<input type="checkbox" name="done" value="1"<?php if ($todo['done']==true) echo " checked"; ?>>
 							<input type="text" name="txt" value="<?php echo htmlspecialchars($todo['txt']); ?>">
-							<input type="submit" name="action" value="Aanpassen">
-							<input type="submit" name="action" value="Verwijderen">
-						</form></div>
+							<input type="image" name="action" src="img/delete.png" value="Verwijderen">
+							<input type="submit" name="action" value="Opslaan">
+						</form><div id="lijn"></div></div>
 					<?php
 						}
 					?>
